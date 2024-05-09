0c6fc81513c8db6f39b55c6fa7ec52cdbfb85b42
Prateek <prateeksagar806@gmail.com>
2024-05-09 22:49:37 +0530

    Creating for establish custom configuration

--- src/main/java/vidyoatmav1/VidyoatmaApplication.java
+++ src/main/java/vidyoatmav1/VidyoatmaApplication.java
@@ -1,13 +1,27 @@
 package vidyoatmav1;
 
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import vidyoatmav1.databaseconnection.DataStaxAstraProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class VidyoatmaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VidyoatmaApplication.class, args);
+	}
+
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+		Path bundle = astraProperties.getSecureBundleConnect().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
 	}
 
 }
