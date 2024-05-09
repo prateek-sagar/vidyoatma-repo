56abf736d9e39aecf911f1df3ce97e10152feec6
Prateek <prateeksagar806@gmail.com>
2024-05-09 23:02:49 +0530

    Authentication Controller helps to call the service method and recieve the request from the user

---
+++ src/main/java/vidyoatmav1/authentication/AuthController.java
@@ -0,0 +1,26 @@
+package vidyoatmav1.authentication;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest authRequest) {
+        return ResponseEntity.ok(authService.authenticate(authRequest));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(authService.save(registerRequest));
+    }
+}
