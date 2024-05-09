e9b1466d523d91b5471a540f1624c3daecd4d414
Prateek <prateeksagar806@gmail.com>
2024-05-09 22:32:41 +0530

    Initilize Project for the benefits of school, teachers and students. They can use this system to reduce their manual efforts. And strengthen the education system.

---
+++ src/main/java/vidyoatmav1/VidyoatmaApplication.java
@@ -0,0 +1,13 @@
+package vidyoatmav1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidyoatmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidyoatmaApplication.class, args);
+	}
+
+}
