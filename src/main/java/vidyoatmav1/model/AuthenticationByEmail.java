c0be8cb65c4eebbda5e5d589b1be19da63176562
Prateek <prateeksagar806@gmail.com>
2024-05-12 10:04:39 +0530

    Renaming the class name for better understanding

---
+++ src/main/java/vidyoatmav1/model/AuthenticationByEmail.java
@@ -0,0 +1,69 @@
+package vidyoatmav1.model;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.core.mapping.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(value = "authentication_by_email")
+public class AuthenticationByEmail implements UserDetails {
+    @PrimaryKeyColumn(value = "loginemail", type = PrimaryKeyType.PARTITIONED)
+    private String loginemail;
+    @Column(value = "loginpass")
+    private String loginpass;
+    @Column
+    private Role role;
+    @Column
+    private UUID id;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.toString()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.loginpass;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.loginemail;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
