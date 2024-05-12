f4a1f4808755ce6146f845086828a7fa034346df
Prateek <prateeksagar806@gmail.com>
2024-05-12 10:13:39 +0530

    Adding more fields to recieve from user while registering instituion

---
+++ src/main/java/vidyoatmav1/authentication/InstitutionRegisterRequest.java
@@ -0,0 +1,29 @@
+package vidyoatmav1.authentication;
+
+import java.util.UUID;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import vidyoatmav1.model.Role;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class InstitutionRegisterRequest {
+    private String username;
+    private String password;
+    private Role role;
+    private UUID id;
+    private String name;
+    private String building_no;
+    private String locality;
+    private String city;
+    private String district;
+    private String state;
+    private String country;
+    private Date establishmentDate;
+    private int lowerStandard;
+    private int higherStandard;
+}
