47c8e4a5d9b948455e8a9cd2cf70f2f568e5ec8b
Prateek <prateeksagar806@gmail.com>
2024-05-13 16:00:28 +0530

    Create generate credentials and also check in database if it is exist or not and generate accordingly

---
+++ src/main/java/vidyoatmav1/support/GenerateAuthentication.java
@@ -0,0 +1,41 @@
+package vidyoatmav1.support;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import vidyoatmav1.model.AuthenticationByUsername;
+import vidyoatmav1.repositories.AuthenticationByNameRepo;
+
+@Component
+@RequiredArgsConstructor
+public class GenerateAuthentication {
+
+    private final AuthenticationByNameRepo authenticationByName;
+
+    public String generateCredentials(String abreviate, int admissionNo, String name) {
+        String addmissionNumberString = Integer.toString(admissionNo);
+        String username = abreviate.toLowerCase() + addmissionNumberString.toLowerCase() + name.toLowerCase();
+        int suffix = 0;
+        boolean isExist = checkExist(username);
+        while (isExist) {
+            username = username + Integer.toString(suffix);
+            isExist = checkExist(username);
+            suffix++;
+        }
+        return username;
+    }
+
+    private boolean checkExist(String username) {
+        /*
+         * check for username in the username table
+         */
+        Optional<AuthenticationByUsername> user = authenticationByName.findByLoginName(username);
+        System.out.println(user);
+        if (user.isPresent())
+            return true;
+
+        return false;
+    }
+}
