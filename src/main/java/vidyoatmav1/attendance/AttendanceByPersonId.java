394b28fe92877be5cf041e7158edde27463bf9cf
Prateek <prateeksagar806@gmail.com>
2024-05-16 10:12:30 +0530

    Adding the table for storing the attendance of a person, it covers the attendance of teachers and students

---
+++ src/main/java/vidyoatmav1/attendance/AttendanceByPersonId.java
@@ -0,0 +1,29 @@
+package vidyoatmav1.attendance;
+
+import java.util.Date;
+import java.util.UUID;
+
+import org.springframework.data.cassandra.core.cql.Ordering;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
+public class AttendanceByPersonId {
+    @PrimaryKeyColumn(value = "personId", type = PrimaryKeyType.PARTITIONED)
+    private UUID personId;
+    @PrimaryKeyColumn(value = "date", type = PrimaryKeyType.CLUSTERED, ordering = Ordering.ASCENDING)
+    private Date date;
+    @Column
+    private boolean isAbsent;
+}
