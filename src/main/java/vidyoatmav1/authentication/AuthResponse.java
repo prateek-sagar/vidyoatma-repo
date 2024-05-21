ee931b5d08b58e79548cdf95e3640b00ba7e2dc9
Prateek <prateeksagar806@gmail.com>
2024-05-21 12:21:44 +0530

    Change of auth response to resp only because of security reasons

--- src/main/java/vidyoatmav1/authentication/AuthResponse.java
+++ src/main/java/vidyoatmav1/authentication/AuthResponse.java
@@ -10,6 +10,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthResponse {
-    private String token;
-    private String refreshToken;
+    private String resp;
 }
