25a60c141d44ddc7c8bb56fc26236d49f56ecb10
Prateek <prateeksagar806@gmail.com>
2024-05-25 14:56:40 +0530

    Adding the code to check for the users' exixtence while authentication

--- src/main/java/vidyoatmav1/authentication/AuthService.java
+++ src/main/java/vidyoatmav1/authentication/AuthService.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import vidyoatmav1.authconfig.JWTService;
 import vidyoatmav1.model.AuthenticationByEmailOrName;
@@ -20,24 +21,34 @@ public class AuthService {
         private final JWTService jwtService;
         private final AuthenticationManager authenticationManager;
         private final PasswordEncoder passwordEncoder;
-        private final SaveToTables saveToTables;
+        // private final SaveToTables saveToTables;
+        private final CookieController cookieController;
 
-        public AuthResponse saveInstitution(InstitutionRegisterRequest registerRequest) {
-                UUID _id = UUID.randomUUID();
-                System.out.println(registerRequest);
-                // create user
-                AuthenticationByEmailOrName user = AuthenticationByEmailOrName.builder()
-                                .loginprincipal(registerRequest.getUsername())
-                                .loginpass(passwordEncoder.encode(registerRequest.getPassword()))
-                                .id(_id)
-                                .role(registerRequest.getRole()).build();
+        public AuthResponse saveInstitution(InstitutionRegisterRequest registerRequest, HttpServletResponse response) {
+                if (!usersnamerepo.existsByLoginprincipal(registerRequest.getUsername())) {
+                        System.out.println("Creating user");
+                        UUID _id = UUID.randomUUID();
+                        System.out.println(registerRequest);
+                        // create user
+                        AuthenticationByEmailOrName user = AuthenticationByEmailOrName.builder()
+                                        .loginprincipal(registerRequest.getUsername())
+                                        .loginpass(passwordEncoder.encode(registerRequest.getPassword()))
+                                        .id(_id)
+                                        .role(registerRequest.getRole()).build();
 
-                saveToTables.saveInstitution(_id, registerRequest);
-                System.out.println(user);
-                // need to save data in users table
-                usersnamerepo.save(user);
-                var token = jwtService.generateToken(user);
-                return AuthResponse.builder().token(token).build();
+                        // usersnamerepo.save(user);
+                        // saveToTables.saveInstitution(_id, registerRequest);
+                        System.out.println(user);
+                        // need to save data in users table
+                        var token = jwtService.generateToken(user);
+                        var refreshToken = jwtService.generateRefreshToken(user);
+                        cookieController.sentHttpOnlyCookie(response, "access-token", token);
+                        cookieController.sentHttpOnlyCookie(response, "refresh-token", refreshToken);
+                        return AuthResponse.builder().resp(token).build();
+                } else {
+                        System.out.println("Exists");
+                        return null;
+                }
         }
 
         public AuthResponse authenticate(AuthRequest authRequest) {
@@ -47,6 +58,6 @@ public class AuthService {
 
                 var user = usersnamerepo.findByLoginprincipal(authRequest.getUsername()).orElseThrow();
                 var token = jwtService.generateToken(user);
-                return AuthResponse.builder().token(token).build();
+                return AuthResponse.builder().resp(token).build();
         }
 }
