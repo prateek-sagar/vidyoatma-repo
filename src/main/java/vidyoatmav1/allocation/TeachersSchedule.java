9be3497ac76194328bced03927b494305615fd33
Prateek <prateeksagar806@gmail.com>
2024-05-25 14:39:05 +0530

    create the teachers schedule table

---
+++ src/main/java/vidyoatmav1/allocation/TeachersSchedule.java
@@ -0,0 +1,33 @@
+package vidyoatmav1.allocation;
+
+import java.util.UUID;
+import java.util.List;
+
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.Indexed;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(value = "teachers_schedule")
+public class TeachersSchedule {
+    @PrimaryKeyColumn(value = "instituion_id", type = PrimaryKeyType.PARTITIONED)
+    @Indexed
+    private UUID institutionId;
+    @PrimaryKeyColumn(type = PrimaryKeyType.PARTITIONED)
+    @Indexed
+    private UUID teacher;
+    @Column
+    private String name;
+    @Column
+    private List<Integer> schedule;
+}
