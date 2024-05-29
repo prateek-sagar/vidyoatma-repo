7ae21b761eb456e27e3ff2db9684e8efe3e6f0bc
Prateek <prateeksagar806@gmail.com>
2024-05-29 13:49:43 +0530

    Annotate it with CrossOrigin and http status changed

--- src/main/java/vidyoatmav1/authentication/AuthController.java
+++ src/main/java/vidyoatmav1/authentication/AuthController.java
@@ -1,26 +1,38 @@
 package vidyoatmav1.authentication;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest authRequest) {
-        return ResponseEntity.ok(authService.authenticate(authRequest));
+    public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest authRequest,
+            HttpServletResponse response) {
+        System.out.println(authRequest);
+        return ResponseEntity.ok(authService.authenticate(authRequest, response));
     }
 
     @PostMapping("/register/institution")
-    public ResponseEntity<AuthResponse> register(@RequestBody InstitutionRegisterRequest registerRequest) {
-        return ResponseEntity.ok(authService.saveInstitution(registerRequest));
+    public HttpStatus register(@RequestBody InstitutionRegisterRequest registerRequest,
+            HttpServletResponse response) {
+
+        var resp = authService.saveInstitution(registerRequest, response);
+        if (resp == null) {
+            return HttpStatus.CONFLICT;
+        }
+        return HttpStatus.CREATED;
     }
 }
