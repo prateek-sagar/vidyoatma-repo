653c20a1356b1ae468724e018c540bafe9b0394e
Prateek <prateeksagar806@gmail.com>
2024-05-29 14:27:19 +0530

    Creating the key for track course table

---
+++ src/main/java/vidyoatmav1/course/key/TrackCourseKey.java
@@ -0,0 +1,32 @@
+package vidyoatmav1.course.key;
+
+import java.util.UUID;
+
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyClass;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import vidyoatmav1.model.tablehelpers.Section;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@PrimaryKeyClass
+public class TrackCourseKey {
+    @PrimaryKeyColumn(value = "institution_id", type = PrimaryKeyType.PARTITIONED)
+    private UUID institutionId;
+    @PrimaryKeyColumn(type = PrimaryKeyType.PARTITIONED)
+    private int standard;
+    @PrimaryKeyColumn(type = PrimaryKeyType.PARTITIONED)
+    private Section section;
+    @PrimaryKeyColumn(value = "subject_name", type = PrimaryKeyType.PARTITIONED)
+    private String subjectName;
+    @PrimaryKeyColumn(type = PrimaryKeyType.CLUSTERED)
+    private int session;
+
+}
