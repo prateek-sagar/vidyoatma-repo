9eccfa826e7ada4111f8c003df65ca2aa192295d
Prateek <prateeksagar806@gmail.com>
2024-05-29 14:28:43 +0530

    As change of track course table so it need to change in repository also

--- src/main/java/vidyoatmav1/course/TrackCourseRepository.java
+++ src/main/java/vidyoatmav1/course/TrackCourseRepository.java
@@ -1,10 +1,11 @@
 package vidyoatmav1.course;
 
-import java.util.UUID;
-
 import org.springframework.data.cassandra.repository.CassandraRepository;
+
+import vidyoatmav1.course.key.TrackCourseKey;
+
 import java.util.List;
 
-public interface TrackCourseRepository extends CassandraRepository<TrackCourse, UUID> {
-    List<TrackCourse> findByInstitutionId(UUID institutionId);
+public interface TrackCourseRepository extends CassandraRepository<TrackCourse, TrackCourseKey> {
+    List<TrackCourse> findByKey(TrackCourseKey key);
 }
