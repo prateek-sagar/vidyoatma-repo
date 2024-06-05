734bf1a4de9d9f796fac6cc3a8ada0d0b2c1ba08
Prateek <prateeksagar806@gmail.com>
2024-06-05 22:10:04 +0530

    Change the response while authentication

--- src/main/java/vidyoatmav1/authentication/AuthResponse.java
+++ src/main/java/vidyoatmav1/authentication/AuthResponse.java
@@ -1,14 +1,20 @@
 package vidyoatmav1.authentication;
 
+import java.util.List;
+import java.util.UUID;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import vidyoatmav1.model.tablehelpers.Role;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthResponse {
-    private String resp;
+    private UUID id;
+    private Role role;
+    private List<String> features;
 }
