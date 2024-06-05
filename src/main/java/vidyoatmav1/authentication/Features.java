43c33d31d12a49b1208cf5fea892d8b18d5af141
Prateek <prateeksagar806@gmail.com>
2024-06-05 07:25:08 +0530

    Create Features for each of role

---
+++ src/main/java/vidyoatmav1/authentication/Features.java
@@ -0,0 +1,47 @@
+package vidyoatmav1.authentication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import vidyoatmav1.model.tablehelpers.Role;
+
+@Service
+@RequiredArgsConstructor
+public class Features {
+
+    private List<String> institution = Stream
+            .of("Student Portfolio", "Track Course", "Notifications", "Attendance", "Finance", "Allocation", "Tsunami")
+            .collect(Collectors.toList());
+
+    private List<String> teachers = Stream
+            .of("Student Portfolio", "Track Course", "Notifications", "Attendance", "Finance", "Allocation", "Tsunami")
+            .collect(Collectors.toList());
+
+    private List<String> student = Stream
+            .of("Portfolio", "Notifications", "Attendance", "Finance", "Allocation")
+            .collect(Collectors.toList());
+
+    public List<String> getFeatures(Role role) {
+        List<String> features = new ArrayList<String>();
+
+        switch (role) {
+            case INSTITUTION:
+                features.addAll(institution);
+                break;
+            case TEACHER:
+                features.addAll(teachers);
+                break;
+            case STUDENT:
+                features.addAll(student);
+                break;
+            default:
+                break;
+        }
+        return features;
+    }
+}
