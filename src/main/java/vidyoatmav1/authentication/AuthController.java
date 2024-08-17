a43e4bbce8fae54296a411c3772f6ba5ab894e87
Prateek <prateeksagar806@gmail.com>
2024-08-17 10:01:31 +0530

    Check for authentication controller for valid credentials

--- src/main/java/vidyoatmav1/authentication/AuthController.java
+++ src/main/java/vidyoatmav1/authentication/AuthController.java
@@ -1,5 +1,6 @@
 package vidyoatmav1.authentication;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,14 +22,20 @@ public class AuthController {
     public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest authRequest,
             HttpServletResponse response) {
         System.out.println(authRequest);
-        return ResponseEntity.ok(authService.authenticate(authRequest, response));
+        boolean isValid = authService.isValidCredentials(authRequest);
+        if (!isValid) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new AuthResponse());
+        }
+        var result = authService.authenticate(authRequest, response);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     @PostMapping("/register/institution")
     public ResponseEntity<AuthResponse> register(@RequestBody InstitutionRegisterRequest registerRequest,
             HttpServletResponse response) {
-
+        System.out.println(registerRequest);
         var resp = authService.saveInstitution(registerRequest, response);
+        System.out.println(resp);
         return ResponseEntity.ok(resp);
     }
 }
