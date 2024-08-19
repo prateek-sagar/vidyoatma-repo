bebc7710893afbc954c8b949439cf18735810973
Prateek <prateeksagar806@gmail.com>
2024-08-19 09:55:02 +0530

    Add the student by section for get student as per section

--- src/main/java/vidyoatmav1/controller/StudentController.java
+++ src/main/java/vidyoatmav1/controller/StudentController.java
@@ -1,5 +1,7 @@
 package vidyoatmav1.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import vidyoatmav1.model.StudentBySection;
 import vidyoatmav1.requests.AddStudentRequest;
+import vidyoatmav1.requests.GetStudentRequest;
 import vidyoatmav1.responses.AfterAddResponse;
 import vidyoatmav1.service.StudentService;
 
@@ -19,6 +23,12 @@ public class StudentController {
 
     @PostMapping("/add")
     public ResponseEntity<AfterAddResponse> saveStudent(@RequestBody AddStudentRequest addrequest) {
+        System.out.println(addrequest);
         return ResponseEntity.ok(studentService.save(addrequest));
+    }
+
+    @PostMapping("/get")
+    public ResponseEntity<List<StudentBySection>> getStudent(@RequestBody GetStudentRequest request) {
+        return ResponseEntity.ok(studentService.getStudents(request));
     }
 }
